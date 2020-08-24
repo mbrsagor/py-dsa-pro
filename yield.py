cfd63f8545774d146fb922bdafcdc00148e7385d
Sagor <mbrsagor@gmail.com>
2020-08-24 20:57:57 +0600

    How to work yield using loop

---
+++ yield.py
@@ -0,0 +1,1 @@
+def square():    i = 1    # An Infinite loop to generate squares    while True:        yield i * i        i += 1  # Next execution resumes# Driver code to test above generatorfor num in square():    if num > 100:        break    print(num)
