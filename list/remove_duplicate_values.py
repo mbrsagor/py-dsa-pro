43ec8c9eaf4ea294fed2de87f2acc2b4d0b5e27b
Sagor <mbrsagor@gmail.com>
2020-10-20 14:08:57 +0600

    Refactor duplicate value remove form list

--- list/remove_duplicate_values.py
+++ list/remove_duplicate_values.py
@@ -1,26 +1,25 @@
-def remove_duplicate_values(item, value):
-    if value == 0 or value == 1:
-        return value
+def remove_duplicate_values(arr, num):
+    if num == 0 or num == 1:
+        return num
 
-    temp = list(range(value))
+    temp = list(range(num))
 
-    # Start traversing elements
     j = 0
-    for i in range(0, value - 1):
-        if item[i] != item[i + 1]:
-            temp[j] = item[i]
+    for i in range(0, num - 1):
+        if arr[i] != arr[i + 1]:
+            temp[j] = arr[i]
             j += 1
 
-    temp[j] = item[value - 1]
+    temp[j] = arr[num - 1]
     j += 1
 
     for i in range(0, j):
-        item[i] = temp[i]
+        arr[i] = temp[i]
     return j
 
 
 items = [1, 2, 2, 3, 4, 4, 4, 5, 5]
 val = len(items)
-val = remove_duplicate_values(items, val)
-for i in range(val):
-    print((items[i]), end=" ")
+_val = remove_duplicate_values(items, val)
+for v in range(_val):
+    print((items[v]), end=" ")
