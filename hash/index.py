aba513effa54ee898caf9b13fdbcab5d7554a42c
Sagor <mbrsagor@gmail.com>
2020-11-01 18:13:09 +0600

    Python hash usecase

---
+++ hash/index.py
@@ -0,0 +1,26 @@
+class Person(object):
+    """
+    Here the `hash` method returns the hash value of an object if it has one.
+    return:: hash() Parameters
+    """
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+    def __eq__(self, other):
+        return self.age == other.age and self.name == other.name
+
+    def __hash__(self):
+        print('The hash is:')
+        return hash((self.age, self.name))
+
+
+person = Person(26, 'Sagor')
+print(hash(person))
+person2 = Person(26, 'Sagor')
+print(hash(person2))
+
+if person == person2:
+    print("They are same")
+else:
+    print("They are different")
