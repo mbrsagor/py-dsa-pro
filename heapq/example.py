847c7e9c76c880fb2d6fa5050a56d2659ffa847c
Sagor <mbrsagor@gmail.com>
2020-11-03 01:42:06 +0600

    Heapq basic example

--- heapq/example.py
+++ heapq/example.py
@@ -1,5 +1,24 @@
-class BinHeap(object):
-    def __init__(self):
-        self.heapList = [0]
-        self.currentSize = 0
+import heapq
 
+
+class PQNode(object):
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    # compares the second value
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __str__(self):
+        return f"{self.key}, {self.value}"
+
+
+_input = [PQNode(1, 4), PQNode(7, 4), PQNode(6, 9), PQNode(2, 5)]
+hin_put = []
+for item in _input:
+    heapq.heappush(hin_put, item)
+
+while hin_put:
+    print(heapq.heappop(hin_put))
